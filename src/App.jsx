d07b99324a95c4252ae111d7beba1963f9ef69ff
Gance123 <110610675+Gance123@users.noreply.github.com>
2022-09-08 01:24:08 +0900

    proceed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
+
   const [imcompleteTodos, setImcompleteTodos] = useState([
     "あいうえお",
     "かきくけこ"
@@ -9,11 +11,33 @@ export const App = () => {
 
   const [completeTodos, setcompleteTodos] = useState(["さしすせそ"]);
 
+  //inputのvalueがテキスト内容を意味する・・・初期値todotext
+  //そのテキスト内容をsetTodoTextで書き換えができるようにする
+  //inputタグのonChangeに関数をいれる・・・onChangeはeventを引数に取る
+  //event.target.valueにテキスト内容が格納されている
+  //・・・setTodoTextで書き換える内容はevent.target.valueを参照するようにする
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  //今までの既に未完了リストにあるtodoと、inputで入ってくるtodoの結合
+  //2週目では、1週目のtodoTextはimcompleteTodosになっている
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...imcompleteTodos, todoText];
+    setImcompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText} //setTodoText(event.target.value)
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className="imcomplete-area">
